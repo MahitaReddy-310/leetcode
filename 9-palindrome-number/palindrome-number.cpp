d946d1ac93e209c407c56af290cec8efb8f654f8
G K MAHITA REDDY <1nc23ci018@ncetmail.com>
2026-07-17 09:28:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (65.75%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        long reverse = 0; 
+
+        while (x > 0) {
+            int remainder = x % 10;
+            reverse = (reverse * 10) + remainder;
+            x = x / 10;
+        }
+
+        return original == reverse;
+    }
+};
