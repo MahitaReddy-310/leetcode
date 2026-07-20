f28839be89c8cf6bd43c98d8b239c7f2294f1dca
G K MAHITA REDDY <1nc23ci018@ncetmail.com>
2026-07-20 09:36:45 +0530

    Time: 3 ms (27.07%) | Memory: 7.6 MB (76.12%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string result="";
+        int n=address.size();
+        for(int i=0;i<n;i++)
+        {
+            if(address[i]=='.')
+            result+="[.]";
+            else
+            result+=address[i];
+        }
+
+        return result;
+
+
+        
+    }
+};
