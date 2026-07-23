ead22f4e69513886232593c3ccd5f32ba95b68c0
G K MAHITA REDDY <1nc23ci018@ncetmail.com>
2026-07-23 15:38:48 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (12.63%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -3,14 +3,12 @@ public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
         int sum=(n*(n+1)/2);
-        int actualsum=0;
+        int actual_sum=0;
         for(int i=0;i<n;i++)
         {
-            actualsum+=nums[i];
+            actual_sum+=nums[i];
         }
-        return sum-actualsum;
-
-
+        return sum-actual_sum;
         
     }
 };
